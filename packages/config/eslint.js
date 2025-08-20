582d0c5eb8aefb77b7bc410c45b4c4b175bf73cf
Marcaexpress <marcaexpress.comercial@gmail.com>
2025-08-20 09:53:04 +0200

    Fix ESLint configuration for all packages - resolve GitHub Actions linting errors

---
+++ packages/config/eslint.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    '@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  },
+  env: {
+    node: true,
+    es6: true
+  }
+};
